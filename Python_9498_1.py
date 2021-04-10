1787339636ba0f098cdcb6a546f3ff8ec0c968f8
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-04-10 15:51:46 +0900

    Python언어 BAEKJOON 9498번 문제 풀기!(1)

--- Python_9498_1.py
+++ Python_9498_1.py
@@ -1,6 +1,6 @@
 # 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
 
-score=int(input())
+score=int(input()) # 사용자로부터 score 입력받기
 
 if(score>=90):
     print('A')
